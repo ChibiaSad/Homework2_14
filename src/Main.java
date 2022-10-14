0b9b8582a024c08a2401c35bb18f764a375a8f18
ChibiaSad <magur1997@gmail.com>
2022-10-14 15:12:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+//        StringList stringList = new StringListImpl(5);
+//        stringList.add("da0");
+//        stringList.add("da");
+//        stringList.add("da2");
+//        System.out.println(stringList);
+//
+//        stringList.add("da");
+//        stringList.add("da2");
+//        System.out.println(stringList);
+//
+//        System.out.println("stringList.add(2, \"net\") = " + stringList.add(2, "net"));
+//        System.out.println(stringList);
+//
+//        stringList.remove("da");
+//        System.out.println(stringList);
+//
+//        stringList.add("end");
+//        System.out.println(stringList);
+    }
+}
